d3a7398021853606f341d50bae10b61fcdd3dd15
Leyla Akmanci <leyla.manci@gmail.com>
2020-07-16 04:36:19 +0300

    first commit
    Signed-off-by: Leyla Akmanci <leyla.manci@gmail.com>

---
+++ app/src/main/java/lyl/manci/androidlifecycle/util/Util.kt
@@ -0,0 +1,19 @@
+package lyl.manci.androidlifecycle.util
+
+import android.util.Log
+
+
+/**       Code with ❤  ´• ل •`   ❤
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+▬     Created by Leyla Akmancı                ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬    ▬
+▬     leyla.manci@gmail.com                           ▬
+▬     ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬     ▬
+▬     16/07/2020 - 04:14        ▬
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+ */
+
+fun String.logError() {
+    Log.e(" MainActivty", this)
+    println("$this is working now on  MainACtivty")
+}
